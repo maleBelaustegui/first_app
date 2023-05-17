3853941e3b67f1e10fb940528bbfd35df6ed7a2e
maleBelaustegui <malebelaustegui@yahoo.com.ar>
2023-05-17 13:14:33 -0300

    firts steps on react n

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'grey',
+        justifyContent: 'center'
+      }}>
+      <Text style= {{
+        fontSize: 55,
+        textAlign: 'center'
+      }}>
+        
+        hola
+        
+        </Text>
+    </View>
+  );
+};
+
+export default App;
